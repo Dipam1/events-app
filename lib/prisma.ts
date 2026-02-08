c4f1c3b31466343917109bb86f6874293a4cc005
dipam1 <deepumpoudel@gmail.com>
2026-02-08 15:25:19 -0500

    Changed the app name and removed boilerplate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -9,4 +9,4 @@ export const prisma =
     datasourceUrl: process.env.DATABASE_URL,
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
